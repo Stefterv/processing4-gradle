3087bcb63e5e1523c2a20f093a73749126df3431
Stef Tervelde <stef@steftervelde.nl>
2024-04-13 13:04:38 +0200

    jetpack compose testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    kotlin("jvm") version "1.9.23"
+    id("org.jetbrains.compose") version "1.6.2" apply false
 }
 
 group = "org.processing"
--- package/build.gradle.kts
+++ package/build.gradle.kts
@@ -1,21 +1,22 @@
 import de.undercouch.gradle.tasks.download.Download
 import io.github.fvarrui.javapackager.model.FileAssociation
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     id("java")
-    id("application")
-    id("io.github.fvarrui.javapackager.plugin")
+//    id("application")
+//    id("io.github.fvarrui.javapackager.plugin")
     id("de.undercouch.download") version "5.6.0"
-//    kotlin("jvm") version "1.9.23"
-//    id("org.jetbrains.compose") version "1.6.2"
+    kotlin("jvm") version "1.9.23"
+    id("org.jetbrains.compose") version "1.6.2"
 }
 
 group = "org.processing"
 version = "4.4"
 
-application {
-    mainClass = "processing.app.Base"
-}
+//application {
+//    mainClass = "processing.app.Base"
+//}
 
 // This can be removed by moving the asset management to native Java resources
 sourceSets{
@@ -26,50 +27,64 @@ sourceSets{
     }
 }
 
-javapackager {
-    mainClass("processing.app.ui.Splash")
+compose.desktop {
+    application {
+        mainClass = "processing.app.ui.Splash"
 
-//    bundleJre(false)
-    // Bundling the JRE is what is causing the missing classes for the autocomplete, it doesn't copy over the jmod files that we need
-    bundleJre(true)
-//    customizedJre(false) // Disabled to enabled debugging on the build package
+        nativeDistributions {
+            includeAllModules = true
 
-    displayName("Processing")
-    name("Processing")
-    url("https://processing.org")
-    //    licenseFile("todo")
-    // TODO: Add after resources folder
-    additionalResources( file("build/resources/main").list()?.map { t->  file("build/resources/main/${t}") }?.toMutableList() ?: mutableListOf())
-    fileAssociations(mutableListOf(
-        FileAssociation().apply {
-            extension = "pde"
-            description = "Processing Source Code"
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.example.project"
+            packageVersion = "1.0.0"
         }
-    ))
-    macConfig.apply {
-        isGeneratePkg = false
-        isGenerateDmg = false
-        appId = "org.processing.app"
-        entitlements = file("assets/mac/processing.entitlements")
-        backgroundImage = file("assets/mac/background.png")
-        windowWidth = 750
-        windowHeight = 450
-        iconX = 50
-        iconSize = 250
-        iconY = (windowHeight - iconSize) / 2 + 35
-        appsLinkIconX = (windowWidth - iconSize)
-        appsLinkIconY = iconY
-        windowX = 200
-        windowY = 200
-    }
-    winConfig.apply {
-        isGenerateMsi = false
-        isGenerateMsm = false
-        fileVersion = project.version as String
-        productVersion = project.version as String
-        isDisableRunAfterInstall = false
     }
 }
+
+//javapackager {
+//    mainClass("processing.app.ui.Splash")
+//
+////    bundleJre(false)
+//    // Bundling the JRE is what is causing the missing classes for the autocomplete, it doesn't copy over the jmod files that we need
+//    bundleJre(true)
+////    customizedJre(false) // Disabled to enabled debugging on the build package
+//
+//    displayName("Processing")
+//    name("Processing")
+//    url("https://processing.org")
+//    //    licenseFile("todo")
+//    // TODO: Add after resources folder
+//    additionalResources( file("build/resources/main").list()?.map { t->  file("build/resources/main/${t}") }?.toMutableList() ?: mutableListOf())
+//    fileAssociations(mutableListOf(
+//        FileAssociation().apply {
+//            extension = "pde"
+//            description = "Processing Source Code"
+//        }
+//    ))
+//    macConfig.apply {
+//        isGeneratePkg = false
+//        isGenerateDmg = false
+//        appId = "org.processing.app"
+//        entitlements = file("assets/mac/processing.entitlements")
+//        backgroundImage = file("assets/mac/background.png")
+//        windowWidth = 750
+//        windowHeight = 450
+//        iconX = 50
+//        iconSize = 250
+//        iconY = (windowHeight - iconSize) / 2 + 35
+//        appsLinkIconX = (windowWidth - iconSize)
+//        appsLinkIconY = iconY
+//        windowX = 200
+//        windowY = 200
+//    }
+//    winConfig.apply {
+//        isGenerateMsi = false
+//        isGenerateMsm = false
+//        fileVersion = project.version as String
+//        productVersion = project.version as String
+//        isDisableRunAfterInstall = false
+//    }
+//}
 
 //kotlin {
 //    jvmToolchain(21)
@@ -89,14 +104,13 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":java"))
 
+//    implementation(compose.desktop.currentOs)
 //    implementation(compose.runtime)
 //    implementation(compose.foundation)
 //    implementation(compose.material)
 //    implementation(compose.ui)
 //    implementation(compose.components.resources)
 //    implementation(compose.components.uiToolingPreview)
-//
-//    implementation(compose.desktop.currentOs)
 }
 
 // This could be removed if the internal build system within processing was moved to gradle
